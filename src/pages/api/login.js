3a927370102fa6a471d60cbabbb7d3ab31d374f6
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-12-21 13:18:30 +0700

    return 200

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -2,9 +2,5 @@ export default function handler(req, res) {
   const userPayload = req.body;
   const { username, password } = userPayload;
   console.log(userPayload)
-  if (username === "admin" && password === "admin") {
-    res.status(200).json({ name: "John Doe" });
-  } else {
-    res.status(401).json({ message: "Unauthorized" });
-  }
+  res.status(200).json({ name: "John Doe" });
 }
