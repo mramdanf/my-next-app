5037a8df1d2891e787f6bd1183a099247429a6f2
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-12-21 13:12:55 +0700

    log user payload

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   const userPayload = req.body;
   const { username, password } = userPayload;
+  console.log(userPayload)
   if (username === "admin" && password === "admin") {
     res.status(200).json({ name: "John Doe" });
   } else {
