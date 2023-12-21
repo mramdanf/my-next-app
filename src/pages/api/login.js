13af91317e6f901e222c6e1319c037a4dccc3369
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-12-21 11:50:24 +0700

    login api

---
+++ src/pages/api/login.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const userPayload = req.body;
+  const { username, password } = userPayload;
+  if (username === "admin" && password === "admin") {
+    res.status(200).json({ name: "John Doe" });
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
